0896f130f47d6a361f3ba599dfcf39e9a96b1834
ChrisHanes3 <87105607+ChrisHanes3@users.noreply.github.com>
2023-04-20 12:28:02 -0400

    Removed print from example .ino file

--- Examples/Motor.ino
+++ Examples/Motor.ino
@@ -2,7 +2,6 @@
 using namespace std;
 //#include "Motor.cpp"
 #include "Motor.h"
-#include "PinMap.h"
 
 
 int percPWM = 0;
@@ -15,9 +14,7 @@ PWM pwm2 = PWM(2, 3, 50, 10);
 Motor motor2 = Motor(41, 42, pwm2);
 PWM pwm3 = PWM(4, 5, 50, 10);
 Motor motor3 = Motor(39, 40, pwm3);*/
-PWM pwm4 = PWM(6, 7, 50, 10);
-Motor motor1 = Motor(37, 38, pwm4);
-Motor motor;
+Motor motor = Motor(2, 1, 0 ,1);
 int numPumps = 1;
 
 void setup() {
@@ -26,11 +23,9 @@ void setup() {
   /*for (i=0; i<motors.length(); i++) {
     motors[i].print();
     motors[i].setDirection(FORWARD);
-    cout << "Setting PWM: " << percPWM << "%"<< endl;
     motors[i].setPWM(percPWM);//set pwm to 0 as a percent
   }
 
-  cout <<"setting pin 5 high" << endl;
   pinMode(5, OUTPUT);//LED1
   digitalWrite(5, HIGH);
   pinMode(4, INPUT);//Button1
@@ -44,7 +39,7 @@ void setup() {
   /*for (int i = 0; i < numPumps; i++) {
     for (int j = IN1; j != TERMINATE; j++) {
       PinType pintype = static_cast<PinType>(j);
-      cout << "pump #" << i+1 << " , enumInd: " << j << " pin Value: " << FetchMappedPin(i, pintype).PinNumber << endl;
+      std::cout << "pump #" << i+1 << " , enumInd: " << j << " pin Value: " << FetchMappedPin(i, pintype).PinNumber << std::endl;
     }
   }*/
 
@@ -55,7 +50,7 @@ void setup() {
     int j = k%5;
     PinType pintype = static_cast<PinType>(j);
 
-    cout << "pump #" << i+1 << " , enumInd: " << j << " pin Value: " << getPin(k).PinNumber << endl;
+    std::cout << "pump #" << i+1 << " , enumInd: " << j << " pin Value: " << getPin(k).PinNumber << std::endl;
     
   }*/
 }
@@ -64,11 +59,10 @@ void loop() {
   //serial input code:
   /*int button1 = digitalRead(4);
   if (button1==HIGH) {
-    cout<<"switch1 High"<<endl;
+    log("Switch1 High");
   } else {
-    cout<<"switch1 Low"<<endl;
+    log("Switch1 Low");
   }
-  cout<<i<<endl;
   */
   if (Serial.available()){
     int temp = Serial.peek();
@@ -79,9 +73,17 @@ void loop() {
     } else if (temp == 70) { //"F"
       motor.setDirection(FORWARD);
       float trash = Serial.parseFloat();
+    } else if (temp == 68) { //"D"
+      motor.disable();
+      float trash = Serial.parseFloat();
+    } else if (temp == 69) { //"E"
+      motor.enable();
+      float trash = Serial.parseFloat();
+    } else if (temp == 80) { //"P"
+      motor.print();
+      float trash = Serial.parseFloat();
     } else {
       percPWM = Serial.parseFloat();
-      cout << "Setting PWM: " << percPWM << "%"<< endl;
       motor.setPWM(percPWM);
     }
   }
