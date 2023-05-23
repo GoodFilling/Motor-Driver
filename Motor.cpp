3c83c850aa80383f66394aae63f1e67ae50b554b
ChrisHanes3 <87105607+ChrisHanes3@users.noreply.github.com>
2023-05-23 10:51:23 -0400

    Bug-27 Added a function to detach LEDC pins (PWM)

--- Motor.cpp
+++ Motor.cpp
@@ -28,6 +28,12 @@ Motor::Motor(int I1P, int I2P, int pwmChannel1, int pwmChannel2) {
 
 // }//Motor destructor - it should automatically destruct when it leaves scope, but keeping this here just in case we find we need it
 
+bool Motor::detach() {
+  ledcDetachPin(this->In1Pin);
+  ledcDetachPin(this->In2Pin);
+  dir = FORWARD;
+}
+
 void Motor::print() {
   //int dir = this->dir;
   log("In1Pin: %i", this->In1Pin);
